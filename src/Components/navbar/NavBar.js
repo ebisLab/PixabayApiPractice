0804eddd3cc068f9e3b75940cc3a9268cd3a2d6f
Eunice Baiden <ebaiden@techie.com>
2019-02-08 23:21:23 -0500

    changed app title

--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
 
-const NavBar = () => <AppBar title="PixaBay App" />;
+const NavBar = () => <AppBar title="Pictures App" />;
 
 
 export default NavBar;
