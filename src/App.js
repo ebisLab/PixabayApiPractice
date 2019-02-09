046b112864b9df2cc5a5bb6a3fc771c2d349bf0b
Eunice Baiden <ebaiden@techie.com>
2019-02-08 23:10:44 -0500

    Added more imports

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import createMuiTheme from'material-ui/styles';
 import NavBar from './Components/navbar/NavBar';
 import Search from './Components/search/Search';
-import red from 'material-ui/styles/colors/';
+import {orange500} from 'material-ui/styles/colors/';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-const theme = createMuiTheme
+const theme = createMuiTheme;
+const muiTheme = getMuiTheme(darkBaseTheme);
 
 class App extends Component {
 
